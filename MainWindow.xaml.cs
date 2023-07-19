42544042fdaa9381f8a943e236d7de2e64ed9d85
Igor3370 <139044610+Igor3370@users.noreply.github.com>
2023-07-19 15:32:32 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace CustomHeaderWindowWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            // отследить изменения ширины всего окна
+            this.SizeChanged += OnWindowSizeChanged;
+            void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
+            {
+                if (e.WidthChanged)
+                {
+                    if (this.WindowState == WindowState.Maximized)
+                    {
+                        nameBorder.BorderThickness = new Thickness(7);
+                        labelMaxmin.Content = "❐";
+                    }
+                    else
+                    {
+                        nameBorder.BorderThickness = new Thickness(2, 1, 2, 2);
+                        labelMaxmin.Content = "☐";
+                    }
+                }
+            }
+        }
+        // обрабатывает наведение на кнопку
+        private void header_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Border border = sender as Border;
+            if (border.Name == "close")
+                border.Background = Brushes.Red;
+            else
+            {
+                border.Background = Brushes.LightSeaGreen;
+                border.Opacity = 0.7;
+            }
+        }
+        // обрабатывает: мыша покидает кнопку
+        private void header_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Border border = sender as Border;
+            border.Background = Brushes.DarkCyan;
+        }
+        // обрабатывает нажатие на кнопку
+        private void header_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Border border = sender as Border;
+            if (border.Name == "close")
+                border.Background = Brushes.LightPink;
+            else
+            {
+                border.Background = Brushes.LightSeaGreen;
+                border.Opacity = 1;
+            }
+        }
+        // управение действием кнопок: закрыть, на весь экран, маленькое окно, свернуть на панель задач
+        private void header_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Border border = sender as Border;
+            if (border.Name == "close")
+                this.Close();
+            else if ((border.Name == "maxmin"))
+            {
+                if (this.WindowState == WindowState.Maximized)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Maximized;
+            }
+            else
+                this.WindowState = WindowState.Minimized;
+        }
+    }
+}
